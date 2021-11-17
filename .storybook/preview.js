6c7975bae02826fa72fea88305cbeac727ac57ff
tancou <github@tancou.me>
2021-11-17 17:21:45 +0100

    Remove assets dependancy

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,11 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import { withCssResources } from '@storybook/addon-cssresources';
 
+import indigoPink from '@angular/material/prebuilt-themes/indigo-pink.css'
+import deeppurpleAmber from '@angular/material/prebuilt-themes/deeppurple-amber.css'
+import pinkBluegrey from '@angular/material/prebuilt-themes/pink-bluegrey.css'
+import purpleGreen from '@angular/material/prebuilt-themes/purple-green.css'
+
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
@@ -12,25 +17,29 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   cssresources: [
     {
-      id: 'indigo-pink',
-      code: '<link rel="stylesheet" href="assets/themes/indigo-pink.css">',
-      picked: true
+      id: `indigo-pink.css`,
+      code: `<style>${indigoPink}</style>`,
+      picked: true,
+      hideCode: true,
     },
     {
-      id: 'deeppurple-amber',
-      code: '<link rel="stylesheet" href="assets/themes/deeppurple-amber.css">',
-      picked: false
+      id: `deeppurple-amber.css`,
+      code: `<style>${deeppurpleAmber}</style>`,
+      picked: false,
+      hideCode: true,
     },
     {
-      id: 'pink-bluegrey',
-      code: '<link rel="stylesheet" href="assets/themes/pink-bluegrey.css">',
-      picked: false
+      id: `pink-bluegrey.css`,
+      code: `<style>${pinkBluegrey}</style>`,
+      picked: false,
+      hideCode: true,
     },
     {
-      id: 'purple-green',
-      code: '<link rel="stylesheet" href="assets/themes/purple-green.css">',
-      picked: false
-    }
+      id: `purple-green.css`,
+      code: `<style>${purpleGreen}</style>`,
+      picked: false,
+      hideCode: true,
+    },
   ],
   controls: {
     matchers: {
